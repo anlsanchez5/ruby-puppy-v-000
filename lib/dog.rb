ffd9e72e824c317b8e5be23921434deb095e2161
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-09-29 00:10:09 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+require 'pry'
+class Dog
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.collect! {|dog| dog.name}
+  end
+end
